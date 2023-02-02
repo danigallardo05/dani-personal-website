6539cb68d491a4959fe842655d306dd4001116b5
Daniela Gallardo <promillennials@gmail.com>
2023-02-01 19:19:00 -0500

    working on grids for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import background from "../images/back.jpg";
-import developer from "../images/developer.png";
+import code from "../images/code.png";
 
 const Home = () => {
   return (
@@ -8,14 +8,32 @@ const Home = () => {
       <img src={background} className="bg" />
 
       <div className="title">
-        <h1><b>Hi I'm Dani</b> </h1>
-        {/* <img src={developer} className="developer" /> */}
+        <h1>
+          <b>Hi I'm Dani</b>{" "}
+        </h1>
       </div>
-    
-    <div className="subtitle">
-        <h4>I'm a Full Stack Web Developer and UX|UI Designer.</h4>
-    </div>
 
+      <div className="subtitle">
+        <h4>I'm a Full Stack Web Developer and UX|UI Designer.</h4>
+      </div>
+
+      <br />
+      <br />
+      <br />
+
+      <div className="secondBlock">
+        <div>
+          <h1>Check out what I have done</h1>
+          <ion-icon name="chevron-down-outline"></ion-icon>
+        </div>
+      </div>
+
+      <div className="thirdBlock">
+        <div></div>
+        <div></div>
+        <div></div>
+        
+      </div>
     </div>
   );
 };
